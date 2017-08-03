8f4c7b2ba010b5c46ffbbf5c55c6f03dc416af17
Header-Dev <headerdev@gmail.com>
2017-08-03 20:52:42 +0700

    initial project

--- FIRDBCrud/ViewController.swift
+++ FIRDBCrud/ViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    
+    
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldGenre:UITextField!
+    @IBOutlet weak var labelMessage:UILabel!
+    
+    @IBOutlet weak var tableViewArtists: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableViewArtists.dataSource = self
+        tableViewArtists.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
